e8b478418fa93948088e8b504315e58e14717bb4
Jeonata <jeonata@gmail.com>
2019-09-30 22:19:51 -0300

    Chore: Add token on requests

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -3,6 +3,7 @@ import { toast } from 'react-toastify';
 
 import history from '~/services/history';
 import api from '~/services/api';
+
 import { signInSuccess, signFailure } from './actions';
 
 export function* signIn({ payload }) {
@@ -20,6 +21,8 @@ export function* signIn({ payload }) {
       toast.error('Usuário não é prestador');
       return;
     }
+
+    api.defaults.headers.Authorization = `Bearer ${token}`;
 
     yield put(signInSuccess(token, user));
 
@@ -49,7 +52,18 @@ export function* signUp({ payload }) {
   }
 }
 
+export function setToken({ payload }) {
+  if (!payload) return;
+
+  const { token } = payload.auth;
+
+  if (token) {
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+  }
+}
+
 export default all([
+  takeLatest('persist/REHYDRATE', setToken),
   takeLatest('@auth/SIGN_IN_REQUEST', signIn),
   takeLatest('@auth/SIGN_UP_REQUEST', signUp),
 ]);
